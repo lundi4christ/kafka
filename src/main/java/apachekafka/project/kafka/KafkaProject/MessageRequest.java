ed7b67c66bc3235c7e0e99b2fd9b07d54c09dc5d
lundi4christ <lundi4christ@gmail.com>
2024-01-21 06:22:27 +0100

    newcommit2

--- src/main/java/apachekafka/project/kafka/KafkaProject/MessageRequest.java
+++ src/main/java/apachekafka/project/kafka/KafkaProject/MessageRequest.java
@@ -1,6 +1,11 @@
 package apachekafka.project.kafka.KafkaProject;
 
+
 public class MessageRequest {
 
+    String message;
 
+    public MessageRequest(String message){
+        this.message=message;
+    }
 }
